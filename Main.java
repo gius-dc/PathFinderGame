98ca24a444771546c56f529c14f59c869dd065d7
gius-dc <74567218+gius-dc@users.noreply.github.com>
2023-02-04 23:31:29 +0100

    Delete Main.java

--- Main.java
+++
@@ -1,8 +0,0 @@
-import com.formdev.flatlaf.FlatLightLaf;
-
-public class Main {
-    public static void main(String[] args) throws IllegalAccessException {
-        FlatLightLaf.setup();
-        MainGUI mainGUI = new MainGUI();
-    }
-}
