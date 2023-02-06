e972b5bc5436047334c884ea4f757dd3b56d3ba6
gius-dc <74567218+gius-dc@users.noreply.github.com>
2023-02-06 21:59:04 +0100

    Update LabyrinthGame.java

--- src/LabyrinthGame.java
+++ src/LabyrinthGame.java
@@ -24,7 +24,13 @@ public class LabyrinthGame extends Labyrinth implements Cloneable {
     @Override
     public LabyrinthGame clone() {
         try {
-            return (LabyrinthGame) super.clone();
+            LabyrinthGame clone = (LabyrinthGame) super.clone();
+            clone.objects = new ArrayList<>(this.objects.size());
+            for (ObjectEntity object : this.objects) {
+                clone.objects.add(object);
+            }
+            clone.state = this.state;
+            return clone;
         } catch (CloneNotSupportedException e) {
             return null;
         }
