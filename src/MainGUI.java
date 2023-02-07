5d0a4a6585e39dc5505230241027a078a1ea2061
gius-dc <giuseppe.dellacorte001@studenti.uniparthenope.it>
2023-02-07 15:45:47 +0100

    prova commit

--- src/MainGUI.java
+++ src/MainGUI.java
@@ -58,6 +58,7 @@ public class MainGUI extends JFrame implements Observer {
      */
 
     public MainGUI() {
+        int ciao; // prova commit
         setContentPane(mainPanel);
         setTitle("Labirinto");
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
